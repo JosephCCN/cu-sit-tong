70a664d3f94a9158df70f568de62a41f6d28e93a
Mariouz <joseph02252003@gmail.com>
2022-12-02 00:17:58 +0800

    able to select the drop down list

--- routes/spider.js
+++ routes/spider.js
@@ -6,9 +6,26 @@ require('dotenv').config();
 router.use(express.urlencoded({extended: false}));
 router.use(express.static(__dirname + '/public'));
 
+const spi = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://cusis.cuhk.edu.hk/psc/CSPRD_9/EMPLOYEE/HRMS/c/CU_SCR_MENU.CU_TMSR801.GBL?Page=CU_TMSR801_ENTRY&Action=U');
+    
+    //login to cusis
+    await page.type('#userNameInput', process.env.CUSIS_AC);
+    await page.type('#passwordInput', process.env.CUSIS_PW);
+    await page.keyboard.press('Enter');  
+
+    await page.waitForTimeout(3000);
+
+    await page.select('select[name="CLASS_SRCH_WRK2_STRM$35$"]', '2270');
+    
+    await browser.close();    
+}
+
 router.get('/', (req, res) => {
     res.send('spidering');
-
+    spi(); 
 })
 
 module.exports = router;
